9cb909824c8031f4dc88b8a218b536abbd90b020
ejemaster <ejemaster@gmx.de>
2020-01-02 23:16:32 +0100

    Dockerize Maven Projekt

---
+++ src/main/java/org/i9Bach/mathUtils/Main.java
@@ -0,0 +1,16 @@
+package org.i9Bach.mathUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Mathutils test = new Mathutils();
+		System.out.println( test.Mod(18, 2));
+		System.out.println(test.Name("Ejangue Guillaume"));
+		System.out.println( test.Div(125, 5));
+		System.out.println(test.Sub(12, 3));
+
+		System.out.println("Alle Teste waren Erfolgreich !");		
+
+	}
+
+}
