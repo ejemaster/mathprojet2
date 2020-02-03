b8725fed881c745e635ca81980ae9959b5fb12a1
ejemaster <ejemaster@gmx.de>
2020-02-03 21:16:44 +0100

    Apassung der Klasse

--- src/main/java/org/i9Bach/mathUtils/Main.java
+++ src/main/java/org/i9Bach/mathUtils/Main.java
@@ -3,15 +3,12 @@ package org.i9Bach.mathUtils;
 public class Main {
 
 	public static void main(String[] args) {
-		//Mathutils test = new Mathutils();
-		MOD test1 = new MOD();
-		NAME test2 = new NAME();
-		DIV test3 = new DIV();
-		SUB test4 = new SUB();
-		System.out.println( test1.Mod(18, 2));
-		System.out.println(test2.Name("Ejangue Guillaume"));
-		System.out.println( test3.Div(125, 5));
-		System.out.println(test4.Sub(12, 3));
+		MathFunktion test = new MathFunktion();
+		
+		System.out.println( test.Mod(18, 2));
+		System.out.println(test.Name("Ejangue Guillaume"));
+		System.out.println( test.Div(125, 5));
+		System.out.println(test.Sub(12, 3));
 
 		System.out.println("Alle Teste waren Erfolgreich !");		
 
